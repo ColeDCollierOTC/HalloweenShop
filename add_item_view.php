555ea543ef2b1d985991b84ab23824ce3ab9bbf7
Cole Collier <cc0900910@otc.edu>
2022-10-19 09:27:43 -0500

    added db file but does not work not data yet. got cart going

---
+++ add_item_view.php
@@ -0,0 +1,48 @@
+
+    <main>
+          
+    </main>
+
+    <div class="card" style="width: 30rem;">
+<div class="card-body">
+    <h5 class="card-title">Movie Store</h5>
+    <h5>Add Item</h5>
+        <form action="." method="post">
+            <div class="mb-3">
+                <input type="hidden" name="action" value="add">
+
+                <label>Name:</label>
+                <select name="productkey">
+                <?php foreach($products as $key => $product) :
+                    $cost = number_format($product['cost'], 2);
+                    $name = $product['name'];
+                    $item = $name . ' ($' . $cost . ')';
+                ?>
+                    <option value="<?php echo $key; ?>">
+                        <?php echo $item; ?>
+                    </option>
+                <?php endforeach; ?>
+                </select><br>
+            </div>
+            <div class="mb-3">
+            <label>Quantity:</label>
+            <select name="itemqty">
+            <?php for($i = 1; $i <= 10; $i++) : ?>
+                <option value="<?php echo $i; ?>">
+                    <?php echo $i; ?>
+                </option>
+            <?php endfor; ?>
+            </select><br>
+
+            <label>&nbsp;</label>
+            </div>
+            <div class="mb-3">
+            <input type="submit" value="Add Item" class="btn btn-primary">
+            </div>
+            
+        </form>
+        <div class="mb-3">
+        <p><a href=".?action=show_cart" class="btn btn-primary">View Cart</a></p>  
+        </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,8 +1,27 @@
+<?php
+// Start session management with a persistent cookie
+$lifetime = 60 * 60 * 24 * 14;    // 2 weeks in seconds
+session_set_cookie_params($lifetime, '/');
+session_start();
+
+// Create a cart array if needed
+if (empty($_SESSION['cartHalloween'])) { $_SESSION['cartHalloween'] = array(); }
+
+// Create a table of products
+$products = array();
+$products['movie-1'] = array('name' => 'movie1', 'cost' => '60.00');
+$products['movie-2'] = array('name' => 'movie2', 'cost' => '59.99');
+$products['movie-3'] = array('name' => 'movie3', 'cost' => '30.00');
+
+?>
+
+
+
 <!doctype html>
 <html lang="en">
 
 <head>
-  <title>Title</title>
+  <title>Halloween Shop</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -18,10 +37,52 @@
     <!-- place navbar here -->
     <?php
         include "./view/navigation.php";
+
     ?>
   </header>
   <main>
 
+    <?php
+        include "cart.php";
+
+        $action = filter_input(INPUT_POST, 'action');
+        if ($action === NULL) {
+            $action = filter_input(INPUT_GET, 'action');
+            if ($action === NULL) {
+                $action = 'show_add_item';
+            }
+        }
+
+        // Add or update cart as needed
+        switch($action) {
+            case 'add':
+                $product_key = filter_input(INPUT_POST, 'productkey');
+                $item_qty = filter_input(INPUT_POST, 'itemqty');
+                add_item($product_key, $item_qty);
+                include('cart_view.php');
+                break;
+            case 'update':
+                $new_qty_list = filter_input(INPUT_POST, 'newqty', FILTER_DEFAULT, 
+                                            FILTER_REQUIRE_ARRAY);
+                foreach($new_qty_list as $key => $qty) {
+                    if ($_SESSION['cartHalloween'][$key]['qty'] != $qty) {
+                        update_item($key, $qty);
+                    }
+                }
+                include('cart_view.php');
+                break;
+            case 'show_cart':
+                include('cart_view.php');
+                break;
+            case 'show_add_item':
+                include('add_item_view.php');
+                break;
+            case 'empty_cart':
+                unset($_SESSION['cartHalloween']);
+                include('cart_view.php');
+                break;
+        }
+    ?>
   </main>
   <footer>
     <!-- place footer here -->
